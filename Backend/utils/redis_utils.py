88dcf4b0beee06fbc3c4abdfc2ce97b813e4df9b
조현준 <wns950404@naver.com>
2025-02-21 02:16:39 +0900

    :hammer: 브랜치 병합

--- Backend/utils/redis_utils.py
+++ Backend/utils/redis_utils.py
@@ -3,7 +3,7 @@ import redis.asyncio as redis
 from redis.exceptions import ConnectionError
 from fastapi import Request
 
-REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
 
